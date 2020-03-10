d5c0bfc2ec9877b71969d0245cce37c8bd11f681
Wendel Fernandes de Lana <48734976+WendelLana@users.noreply.github.com>
2020-03-10 17:25:14 -0300

    Create getData.js

---
+++ getData.js
@@ -0,0 +1,31 @@
+
+
+var req = new XMLHttpRequest(); 
+req.open("GET", "https://ax4bc.crm.dynamics.com/api/data/v9.1/accounts?$select=name,emailaddress1,address1_country", true);
+req.setRequestHeader("OData-MaxVersion", "4.0");
+req.setRequestHeader("OData-Version", "4.0");
+req.setRequestHeader("Accept", "application/json");
+req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+req.setRequestHeader("Prefer", "odata.include-annotations=\"*\"");
+var tempArray = new array();
+req.onreadystatechange = function() {
+    if (this.readyState === 4) {
+        req.onreadystatechange = null;
+        if (this.status === 200) {
+            var results = JSON.parse(this.response);
+            for (var i = 0; i < results.value.length; i++) {
+                var table = document.getElementById("tabelaCRM");
+                var row = table.insertRow(0);
+                var cell1 = row.insertCell(0);
+                var cell2 = row.insertCell(1);
+                var country = row.insertCell(2);
+                name.innerHTML = results.value[i]["name"];
+                email.innerHTML = results.value[i]["emailaddress1"];
+                country.innerHTML = results.value[i]["address1_country"];
+                }
+        }  else {
+            Xrm.Utility.alertDialog(this.statusText);
+        }
+    }
+};
+req.send();
