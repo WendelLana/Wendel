4dd0e14bf7a5a720d773843654afdca26619748f
Wendel Fernandes de Lana <48734976+WendelLana@users.noreply.github.com>
2020-03-10 17:26:58 -0300

    Update getData.js

--- getData.js
+++ getData.js
@@ -1,5 +1,5 @@
 
-
+console.log("worked");
 var req = new XMLHttpRequest(); 
 req.open("GET", "https://ax4bc.crm.dynamics.com/api/data/v9.1/accounts?$select=name,emailaddress1,address1_country", true);
 req.setRequestHeader("OData-MaxVersion", "4.0");
@@ -16,16 +16,14 @@ req.onreadystatechange = function() {
             for (var i = 0; i < results.value.length; i++) {
                 var table = document.getElementById("tabelaCRM");
                 var row = table.insertRow(0);
-                var cell1 = row.insertCell(0);
-                var cell2 = row.insertCell(1);
+                var name = row.insertCell(0);
+                var email = row.insertCell(1);
                 var country = row.insertCell(2);
                 name.innerHTML = results.value[i]["name"];
                 email.innerHTML = results.value[i]["emailaddress1"];
                 country.innerHTML = results.value[i]["address1_country"];
                 }
-        }  else {
-            Xrm.Utility.alertDialog(this.statusText);
-        }
+        }        
     }
 };
 req.send();
