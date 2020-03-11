a1d6a96d2ad491cf6e95aedb779b6f3016c15793
Wendel Fernandes de Lana <48734976+WendelLana@users.noreply.github.com>
2020-03-11 13:38:15 -0300

    Update getData.js

--- getData.js
+++ getData.js
@@ -1,5 +1,6 @@
-var req = new XMLHttpRequest(); 
-req.open("GET", "https://dummy.restapiexample.com/api/v1/employees", true);
+var req = new HttpClient();
+            HttpResponseMessage result = req.GetAsync("https://dummy.restapiexample.com/api/v1/employees").Result;            
+            var stringJson = result.Content.ReadAsStringAsync().Result;
 req.setRequestHeader("OData-MaxVersion", "4.0");
 req.setRequestHeader("OData-Version", "4.0");
 req.setRequestHeader("Accept", "application/json");
@@ -8,18 +9,17 @@ req.setRequestHeader("Prefer", "odata.include-annotations=\"*\"");
 req.onreadystatechange = function() {
     if (this.readyState === 4) {
         req.onreadystatechange = null;
-        if (this.status === 200) {
-            var results = JSON.parse(this.responseText);
-            for (var i = 0; i < results.value.length; i++) {
+        if (this.status === 200) {            
+            for (var i = 0; i < stringJson.value.length; i++) {
                 var table = document.getElementById("tabelaCRM");
                 var row = table.insertRow(0);
                 var name = row.insertCell(0);
                 var email = row.insertCell(1);
                 var country = row.insertCell(2);
-                console.log(results.value[i]["employee_name"]);
-                name.innerHTML = results.value[i]["employee_name"];
-                email.innerHTML = results.value[i]["employee_salary"];
-                country.innerHTML = results.value[i]["employee_age"];
+                console.log(stringJson.value[i]["employee_name"]);
+                name.innerHTML = stringJson.value[i]["employee_name"];
+                email.innerHTML = stringJson.value[i]["employee_salary"];
+                country.innerHTML = stringJson.value[i]["employee_age"];
             }        
         }
     }
