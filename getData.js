66adca81d4b4634a6fd6c3776d6d854cae53126a
Wendel Fernandes de Lana <48734976+WendelLana@users.noreply.github.com>
2020-03-11 14:22:11 -0300

    Update getData.js

--- getData.js
+++ getData.js
@@ -1,27 +1,23 @@
-var req = new HttpClient();
-            HttpResponseMessage result = req.GetAsync("https://dummy.restapiexample.com/api/v1/employees").Result;            
-            var stringJson = result.Content.ReadAsStringAsync().Result;
+var req = new XMLHttpRequest(); 
+req.open("GET", "https://dummy.restapiexample.com/api/v1/employees", true);
 req.setRequestHeader("OData-MaxVersion", "4.0");
 req.setRequestHeader("OData-Version", "4.0");
 req.setRequestHeader("Accept", "application/json");
 req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
 req.setRequestHeader("Prefer", "odata.include-annotations=\"*\"");
 req.onreadystatechange = function() {
-    if (this.readyState === 4) {
-        req.onreadystatechange = null;
-        if (this.status === 200) {            
-            for (var i = 0; i < stringJson.value.length; i++) {
+    if (this.readyState === 4 && this.status === 200) { 
+            var result = JSON.parse(req.responseText).data;            
+            for (var i = 0; i < result.length; i++) {
                 var table = document.getElementById("tabelaCRM");
                 var row = table.insertRow(0);
                 var name = row.insertCell(0);
                 var email = row.insertCell(1);
                 var country = row.insertCell(2);
-                console.log(stringJson.value[i]["employee_name"]);
-                name.innerHTML = stringJson.value[i]["employee_name"];
-                email.innerHTML = stringJson.value[i]["employee_salary"];
-                country.innerHTML = stringJson.value[i]["employee_age"];
+                name.innerHTML = result[i]["employee_name"];
+                email.innerHTML = result[i]["employee_salary"];
+                country.innerHTML = result[i]["employee_age"];
             }        
         }
-    }
-};
+    };
 req.send();
