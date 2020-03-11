998815e3842d15dfc3b856f57e661eacddaf8d22
Wendel Fernandes de Lana <48734976+WendelLana@users.noreply.github.com>
2020-03-11 13:26:21 -0300

    Update getData.js

--- getData.js
+++ getData.js
@@ -1,5 +1,5 @@
 var req = new XMLHttpRequest(); 
-req.open("GET", "https://ax4bc.crm.dynamics.com/api/data/v9.1/accounts?$select=name,emailaddress1,address1_country", true);
+req.open("GET", "dummy.restapiexample.com/api/v1/employees", true);
 req.setRequestHeader("OData-MaxVersion", "4.0");
 req.setRequestHeader("OData-Version", "4.0");
 req.setRequestHeader("Accept", "application/json");
@@ -16,12 +16,12 @@ req.onreadystatechange = function() {
                 var name = row.insertCell(0);
                 var email = row.insertCell(1);
                 var country = row.insertCell(2);
-                console.log(results.value[i]["name"]);
-                name.innerHTML = results.value[i]["name"];
-                email.innerHTML = results.value[i]["emailaddress1"];
-                country.innerHTML = results.value[i]["address1_country"];
-                }
-        }        
+                console.log(results.value[i]["employee_name"]);
+                name.innerHTML = results.value[i]["employee_name"];
+                email.innerHTML = results.value[i]["employee_salary"];
+                country.innerHTML = results.value[i]["employee_age"];
+            }        
+        }
     }
 };
 req.send();
